45cccc3e43690aa79b1f667114c3374d27df5108
szekelyzol <szekelyzol@gmail.com>
2025-08-12 02:47:40 +0200

    Remove this dumb page

--- pages/download.tsx
+++
@@ -1,9 +0,0 @@
-function DownloadPage() {
-  return React.createElement('div', null, 
-    React.createElement('h1', null, 'Download Page'),
-    React.createElement('p', null, 'Setup instructions coming soon.'),
-    React.createElement('a', { href: '/' }, 'Back to Home')
-  );
-}
-
-export default DownloadPage;
