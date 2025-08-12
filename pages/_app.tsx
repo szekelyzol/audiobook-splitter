602bfa022caba1f4889ef1c895e1d57f3f3b2d2d
szekelyzol <szekelyzol@gmail.com>
2025-08-12 02:54:33 +0200

    Fix app.tsx page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,5 @@
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#007bff" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
