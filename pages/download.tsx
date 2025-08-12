fa4cb32b502ea1bbc405985455febc002d008eea
szekelyzol <szekelyzol@gmail.com>
2025-08-12 02:43:54 +0200

    Fix download page again and again

--- pages/download.tsx
+++ pages/download.tsx
@@ -1,11 +1,17 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function DownloadPage() {
   return (
-    <div>
-      <h1>Download Page</h1>
-      <p>Setup instructions coming soon.</p>
-      <Link href="/">Back to Home</Link>
-    </div>
+    <>
+      <Head>
+        <title>Download - Audiobook Splitter</title>
+      </Head>
+      <div>
+        <h1>Download Page</h1>
+        <p>Setup instructions coming soon.</p>
+        <Link href="/">Back to Home</Link>
+      </div>
+    </>
   );
 }
