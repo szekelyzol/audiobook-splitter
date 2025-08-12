c14008f526f3b4f59d80c1bfe8b1493c8c180c45
szekelyzol <szekelyzol@gmail.com>
2025-08-12 03:26:07 +0200

    Test again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,3 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Audiobook Splitter Works!</h1>
-      <p>Deployment successful</p>
-    </div>
-  )
+  return <h1>Hello World</h1>
 }
