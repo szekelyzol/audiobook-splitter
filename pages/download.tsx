4db384449936b01ce77c3cf56f323b44fed6379e
szekelyzol <szekelyzol@gmail.com>
2025-08-12 02:38:24 +0200

    Fix download page again

--- pages/download.tsx
+++ pages/download.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
+
 export default function DownloadPage() {
   return (
     <div>
       <h1>Download Page</h1>
       <p>Setup instructions coming soon.</p>
-      <a href="/">Back to Home</a>
+      <Link href="/">Back to Home</Link>
     </div>
   );
 }
