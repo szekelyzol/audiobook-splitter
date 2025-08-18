1d57123f45a18d19acb4c3061c8a7da2bb6fd247
szekelyzol <szekelyzol@gmail.com>
2025-08-18 23:13:43 +0200

    configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  output: 'export',
+  // Replace 'your-repo-name' with your actual repository name
+  basePath: process.env.NODE_ENV === 'production' ? '/audiobook-splitter' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/audiobook-splitter/' : '',
+};
